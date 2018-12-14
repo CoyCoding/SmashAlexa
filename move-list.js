76b74c48028775b06099cd7843721a6fb810eae7
Matt Coy <40921499+fluffytoycoy@users.noreply.github.com>
2018-12-14 16:55:53 -0600

    Update and rename recipes.js to move-list.js
    added more characters changed name

---
+++ move-list.js
@@ -0,0 +1,90 @@
+/* eslint-disable  func-names */
+/* eslint-disable max-len */
+/* eslint quote-props: ['error', 'consistent']*/
+module.exports = {
+    'RECIPE_EN_GB': {
+        
+    },
+    'MOVE_LIST_EN_US': {
+        'captain falcon_down b': 'I know you asked for captain falcons down b',
+        'captain falcon_up b': 'I know you asked for captain falcons up b',
+        'captain falcon_b': 'I know you asked for captain falcons b special',
+        'captain falcon_side b': 'I know you asked for captain falcons side b',
+        
+        'fox_down b': 'I know you asked for foxs down b',
+        'fox_up b': 'I know you asked for foxs up b ',
+        'fox_b': 'I know you asked for foxs b special',
+        'fox_side b': 'I know you asked for foxs down side b',
+        
+        'bowser_down b': 'The Bowser Bomb, Bowser slams the ground with his fat ass',
+        'bowser_up b': 'The Whirling Fortress, Bowser retreats into his shell and spins like a motha\' fucka\'',
+        'bowser_b': 'Fire Breath, bowser blows his firey stank breath all over the opponent. Don\'t worry the stink slowly loses power and distance' ,
+        'bowser_side b': 'The KOOPA KLAW, bowser swings his big ass claw at the opponent. Depending on the range, you will either grab the opponent and bite their head, or knock them away with a mighty blow',
+        
+        'doctor mario_down b': 'The Dr. Tornado, dr. Mario spins around in a circle. While this move looks exactly like Regular Marios Tornado, enemies hit by this move will be launched to the left or right.',
+        'doctor mario_up b': 'The Super Jump Punch, wow its the same as marios up b what a suprise',
+        'doctor mario_b': 'The Mega Vitamins, mario prescribes the pain by throwing out a pill',
+        'doctor mario_side b': 'The Super Sheet, Dr. mario whips his small pox infected sheet at his enemies. The sheet will not only reflect projectiles, but will also turn opponents around.',
+        
+        'donkey kong_down b': 'The Ground Slap, Donkey Kong repeatedly slaps the ground looking for bananas. To bad bananas grow on trees, and not in the ground.',
+        'donkey kong_up b': 'The Spinning Kong, Donkey Kong streches out his arms and pretends to be a helicopter. This isn\'t a very effective form of flight',
+        'donkey kong_b': 'The Giant Punch, Donkey Kong stands and place and charges up one hell of a punch.',
+        'donkey kong_side b': 'The Headbutt, Donkey Kong slams his head into an opponent, burying them in the ground.',
+        
+        '_down b': 'I know you asked for foxs down b',
+        '_up b': 'I know you asked for foxs up b ',
+        '_b': 'I know you asked for foxs b special',
+        '_side b': 'I know you asked for foxs down side b',
+        
+                '_down b': 'I know you asked for foxs down b',
+        '_up b': 'I know you asked for foxs up b ',
+        '_b': 'I know you asked for foxs b special',
+        '_side b': 'I know you asked for foxs down side b',
+        
+                '_down b': 'I know you asked for foxs down b',
+        '_up b': 'I know you asked for foxs up b ',
+        '_b': 'I know you asked for foxs b special',
+        '_side b': 'I know you asked for foxs down side b',
+        
+                '_down b': 'I know you asked for foxs down b',
+        '_up b': 'I know you asked for foxs up b ',
+        '_b': 'I know you asked for foxs b special',
+        '_side b': 'I know you asked for foxs down side b',
+        
+                '_down b': 'I know you asked for foxs down b',
+        '_up b': 'I know you asked for foxs up b ',
+        '_b': 'I know you asked for foxs b special',
+        '_side b': 'I know you asked for foxs down side b',
+        
+                '_down b': 'I know you asked for foxs down b',
+        '_up b': 'I know you asked for foxs up b ',
+        '_b': 'I know you asked for foxs b special',
+        '_side b': 'I know you asked for foxs down side b',
+        
+                '_down b': 'I know you asked for foxs down b',
+        '_up b': 'I know you asked for foxs up b ',
+        '_b': 'I know you asked for foxs b special',
+        '_side b': 'I know you asked for foxs down side b',
+        
+                '_down b': 'I know you asked for foxs down b',
+        '_up b': 'I know you asked for foxs up b ',
+        '_b': 'I know you asked for foxs b special',
+        '_side b': 'I know you asked for foxs down side b',
+        
+                '_down b': 'I know you asked for foxs down b',
+        '_up b': 'I know you asked for foxs up b ',
+        '_b': 'I know you asked for foxs b special',
+        '_side b': 'I know you asked for foxs down side b',
+        
+                '_down b': 'I know you asked for foxs down b',
+        '_up b': 'I know you asked for foxs up b ',
+        '_b': 'I know you asked for foxs b special',
+        '_side b': 'I know you asked for foxs down side b',
+        
+        
+
+    },
+    'RECIPE_DE_DE': {
+
+    },
+};
